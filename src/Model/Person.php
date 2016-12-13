cf9584245a18f504bb12caf221d7f9aa75811d9b
holoflins <hulot.morgan@neuf.fr>
2016-12-13 17:50:07 +0100

    feat: add entity Person

---
+++ src/Model/Person.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Model;
+
+class Person
+{
+    /** @var  string */
+    private $firstname;
+
+    /** @var  string */
+    private $lastname;
+
+    /** @var  string */
+    private $email;
+
+    /** @var  string */
+    private $githubAccount;
+}
