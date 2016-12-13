c39fb68666a837376b0a39213ab47322807f69af
holoflins <hulot.morgan@neuf.fr>
2016-12-13 18:00:17 +0100

    add methods

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -4,4 +4,23 @@ namespace Controller;
 
 class PersonController
 {
+    public function getPerson()
+    {
+        // person has been get
+    }
+
+    public function addPerson()
+    {
+        // person has been added
+    }
+
+    public function deletePerson()
+    {
+        // person has been deleted
+    }
+
+    public function updatePerson()
+    {
+        // person has been updated
+    }
 }
